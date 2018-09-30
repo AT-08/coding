0cefa1509f5a211d23b0f20fcea0e3d1a19cdd85
melvicaballero <melvi.caballero@gmail.com>
2018-09-30 11:32:10 -0400

    Solution to Kata (#8)
    * Solution
    
    * refactor
    
    * stop-gninnips-my-sdrow reviewed

---
+++ src/test/java/org/fundacionjala/coding/SpinWordsTest.java
@@ -0,0 +1,37 @@
+package org.fundacionjala.coding;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ */
+    public class SpinWordsTest {
+
+        private SpinWords spinWords;
+
+    /**
+     *
+     */
+    @Before
+        public void setUp() {
+            spinWords = new SpinWords();
+        }
+
+        /**
+         *
+         */
+        @Test
+        public void testSpinWordsGretaerThanFive() {
+            assertEquals("emocleW", spinWords.spinWords("Welcome"));
+        }
+
+    /**
+     *
+     */
+    @Test
+    public void testSpinWordsGretaerThanFiveSentence() {
+        assertEquals("Hey wollef sroirraw", spinWords.spinWords("Hey fellow warriors"));
+    }
+
+    }
