e1c6ce4add3ec8a7d3b361b19b0843b71bdae7a5
melvicaballero <melvi.caballero@gmail.com>
2018-09-30 11:31:25 -0400

    Melvi/kata/vowel count (#7)
    * kata finished
    
    * gradle fix
    
    * Update Vowels.java

---
+++ src/test/java/org/fundacionjala/coding/VowelsTest.java
@@ -0,0 +1,20 @@
+package org.fundacionjala.coding;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test for {@link Vowels}.
+ */
+public class VowelsTest {
+
+    /**
+     * Verify correct answer.
+     */
+        @Test
+        public void testCase1() {
+            final int expected = 5;
+            assertEquals("Nope!", expected, Vowels.getCount("abracadabra"));
+        }
+
+    }
+
