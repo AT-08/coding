90c02af724cb2f2d6aa2df9951cdd321a7c4267f
Carledriss Zeiss <carledriss@gmail.com>
2018-10-06 15:36:57 -0400

    Refactor code (#54)

--- src/main/java/org/fundacionjala/coding/nestor/SortInnerContent.java
+++ src/main/java/org/fundacionjala/coding/nestor/SortInnerContent.java
@@ -9,33 +9,29 @@ import java.util.stream.Collectors;
  */
 public class SortInnerContent {
 
-  /**
-   * Main method.
-   *
-   * @param words type string.
-   * @return a new string with inner ordered.
-   */
-  public String sortTheInnerContent(final String words) {
-    if (words.length() > 0) {
-      return Arrays.stream(words.split(" "))
-        .map(e -> e.length() > 1 ? e.substring(0, 1)
-          .concat(sortString(e.substring(1, e.length() - 1)))
-          .concat(e.substring(e.length() - 1)) : e)
-        .collect(Collectors.joining(" "));
+    /**
+     * Main method.
+     *
+     * @param words type string.
+     * @return a new string with inner ordered.
+     */
+    public String sortTheInnerContent(final String words) {
+        return Arrays.stream(words.split(" "))
+                .map(e -> e.length() > 2 ? e.substring(0, 1)
+                        .concat(sortString(e.substring(1, e.length() - 1)))
+                        .concat(e.substring(e.length() - 1)) : e)
+                .collect(Collectors.joining(" "));
     }
 
-    return words;
-  }
-
-  /**
-   * Helper to sort inner container string.
-   *
-   * @param inputString Type String.
-   * @return string ordered.
-   */
-  protected static String sortString(final String inputString) {
-    String[] tempArray = inputString.split("");
-    Arrays.sort(tempArray, Comparator.reverseOrder());
-    return String.join("", tempArray);
-  }
+    /**
+     * Helper to sort inner container string.
+     *
+     * @param inputString Type String.
+     * @return string ordered.
+     */
+    private static String sortString(final String inputString) {
+        String[] tempArray = inputString.split("");
+        Arrays.sort(tempArray, Comparator.reverseOrder());
+        return String.join("", tempArray);
+    }
 }
