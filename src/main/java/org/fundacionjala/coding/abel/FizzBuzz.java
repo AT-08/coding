a215de18de697d58de028f5ba5a804baf4182b8c
geremygustavo <abelmallcu123@gmail.com>
2018-10-07 23:56:00 -0400

    new commit

---
+++ src/main/java/org/fundacionjala/coding/abel/FizzBuzz.java
@@ -0,0 +1,13 @@
+package org.fundacionjala.coding.abel;
+
+public class FizzBuzz {
+
+
+
+
+    public String fizzBuzzz(){
+
+
+        return null;
+    }
+}
---
+++ src/test/java/org/fundacionjala/coding/abel/FizzBuzzTest.java
@@ -0,0 +1,18 @@
+package org.fundacionjala.coding.abel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzTest {
+
+
+    @Test
+
+    public void test(){
+
+
+        
+    }
+
+}
