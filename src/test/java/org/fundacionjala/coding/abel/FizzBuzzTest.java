9f08a31e8252962975d438364f0d0ee8d12d7659
geremygustavo <abelmallcu123@gmail.com>
2018-10-08 00:12:11 -0400

    corrections of unit test

--- src/test/java/org/fundacionjala/coding/abel/FizzBuzzTest.java
+++ src/test/java/org/fundacionjala/coding/abel/FizzBuzzTest.java
@@ -18,6 +18,10 @@ public class FizzBuzzTest {
     private List<String> list = new ArrayList<String>();
     private List<String> list2 = new ArrayList<String>();
 
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final String FIZZ_BUZZ = "FizzBuzz";
+
     /**
      *
      */
@@ -34,7 +38,7 @@ public class FizzBuzzTest {
     @Test
     public void test() {
         final int num = 2;
-        assertEquals("Fizz", list.get(num));
+        assertEquals(FIZZ, list.get(num));
     }
 
     /**
@@ -43,7 +47,7 @@ public class FizzBuzzTest {
     @Test
     public void test2() {
         final int num2 = 4;
-        assertEquals("Buzz", list.get(num2));
+        assertEquals(BUZZ, list.get(num2));
     }
 
     /**
@@ -52,7 +56,7 @@ public class FizzBuzzTest {
     @Test
     public void test3() {
         final int num3 = 14;
-        assertEquals("FizzBuzz", list.get(num3));
+        assertEquals(FIZZ_BUZZ, list.get(num3));
     }
 
     /**
@@ -61,7 +65,7 @@ public class FizzBuzzTest {
     @Test
     public void test4() {
         final int num3 = 59;
-        assertEquals("FizzBuzz", list.get(num3));
+        assertEquals(FIZZ_BUZZ, list.get(num3));
     }
 
     /**
@@ -70,7 +74,7 @@ public class FizzBuzzTest {
     @Test
     public void test5() {
         final int num3 = 12;
-        assertEquals("Fizz", list2.get(num3));
+        assertEquals(FIZZ, list2.get(num3));
     }
 
     /**
@@ -79,7 +83,7 @@ public class FizzBuzzTest {
     @Test
     public void test6() {
         final int num3 = 35;
-        assertEquals("Fizz", list2.get(num3));
+        assertEquals(FIZZ, list2.get(num3));
     }
 
     /**
@@ -88,7 +92,7 @@ public class FizzBuzzTest {
     @Test
     public void test7() {
         final int num3 = 24;
-        assertEquals("Buzz", list2.get(num3));
+        assertEquals(BUZZ, list2.get(num3));
     }
 
     /**
