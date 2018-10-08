c790a640e6a249e7ba5fb251502cf2bd919c8f4e
geremygustavo <abelmallcu123@gmail.com>
2018-10-08 00:00:18 -0400

    commit again

--- src/main/java/org/fundacionjala/coding/abel/FizzBuzz.java
+++ src/main/java/org/fundacionjala/coding/abel/FizzBuzz.java
@@ -1,13 +1,50 @@
 package org.fundacionjala.coding.abel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
 public class FizzBuzz {
 
+    private static final int FIZZ_NUM = 3;
+    private static final int BUZZ_NUM = 5;
+    private static final String FIZZBUZZ = "FizzBuzz";
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+    private static final int LENGTH = 100;
 
+    /**
+     * @return value of return
+     */
+    public List<String> fizzbuzz() {
+        List<String> list = new ArrayList<String>();
+        for (int i = 1; i <= LENGTH; i++) {
+            list.add(i % FIZZ_NUM == 0 && i % BUZZ_NUM == 0 ? FIZZBUZZ
+                    : i % FIZZ_NUM == 0 ? FIZZ
+                    : i % BUZZ_NUM == 0 ? BUZZ
+                    : String.valueOf(i));
+        }
+        return list;
+    }
 
+    /**
+     *
+     * @return VALUE OF RETURN
+     */
+    public List<String> fizzBuzz2() {
+        List<String> list = new ArrayList<String>();
+        final int num1 = 53;
+        final int num2 = 35;
+        final int num3 = 0;
+        for (int i = 1; i <= LENGTH; i++) {
 
-    public String fizzBuzzz(){
-
-
-        return null;
+            list.add(i == num1 || i == num2 ? FIZZBUZZ
+                    : i % FIZZ_NUM == num3 || String.valueOf(i).contains("3") ? FIZZ
+                    : i % FIZZ_NUM == num3 || String.valueOf(i).contains("5") ? BUZZ
+                    : String.valueOf(i));
+        }
+        return list;
     }
 }
--- src/test/java/org/fundacionjala/coding/abel/FizzBuzzTest.java
+++ src/test/java/org/fundacionjala/coding/abel/FizzBuzzTest.java
@@ -1,18 +1,104 @@
 package org.fundacionjala.coding.abel;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ *
+ */
 public class FizzBuzzTest {
 
 
+    private FizzBuzz instance;
+    private List<String> list = new ArrayList<String>();
+    private List<String> list2 = new ArrayList<String>();
+
+    /**
+     *
+     */
+    @Before
+    public void before() {
+        instance = new FizzBuzz();
+        list = instance.fizzbuzz();
+        list2 = instance.fizzBuzz2();
+    }
+
+    /**
+     *
+     */
     @Test
+    public void test() {
+        final int num = 2;
+        assertEquals("Fizz", list.get(num));
+    }
 
-    public void test(){
+    /**
+     *
+     */
+    @Test
+    public void test2() {
+        final int num2 = 4;
+        assertEquals("Buzz", list.get(num2));
+    }
 
+    /**
+     *
+     */
+    @Test
+    public void test3() {
+        final int num3 = 14;
+        assertEquals("FizzBuzz", list.get(num3));
+    }
 
-        
+    /**
+     *
+     */
+    @Test
+    public void test4() {
+        final int num3 = 59;
+        assertEquals("FizzBuzz", list.get(num3));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void test5() {
+        final int num3 = 12;
+        assertEquals("Fizz", list2.get(num3));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void test6() {
+        final int num3 = 35;
+        assertEquals("Fizz", list2.get(num3));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void test7() {
+        final int num3 = 24;
+        assertEquals("Buzz", list2.get(num3));
+    }
+
+    /**
+     *
+     */
+
+    @Test
+    public void test8() {
+        final int num3 = 52;
+        assertEquals("FizzBuzz", list2.get(num3));
     }
 
 }
