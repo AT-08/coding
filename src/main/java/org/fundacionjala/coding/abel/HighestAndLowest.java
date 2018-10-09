2619a87522c94c3790bb77ff65ce405280749d24
geremygustavo <38267134+geremygustavo@users.noreply.github.com>
2018-10-09 19:00:33 -0400

    Abel/kata highest and lowest (#79)
    * first commit abel
    
    * commit new
    
    *  commit

---
+++ src/main/java/org/fundacionjala/coding/abel/HighestAndLowest.java
@@ -0,0 +1,30 @@
+package org.fundacionjala.coding.abel;
+
+
+/**
+ *
+ */
+public class HighestAndLowest {
+
+    /**
+     * @param numbers sdfsd
+     * @return sdfsd
+     */
+
+    public String highAndLow(final String numbers) {
+        String[] container = numbers.split(" ");
+        int mini = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < container.length; i++) {
+            int num = Integer.parseInt(container[i]);
+            if (num < mini) {
+                mini = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        return String.format("%d %d", max, mini);
+    }
+}
