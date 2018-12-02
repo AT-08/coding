da2e857bbbc6510b02194f6b3480712659105909
DennisMont <dmmv429@gmail.com>
2018-12-02 16:56:56 -0400

    refactor

--- src/main/java/org/fundacionjala/coding/dennis/DigitalRoot.java
+++ src/main/java/org/fundacionjala/coding/dennis/DigitalRoot.java
@@ -1,4 +1,49 @@
 package org.fundacionjala.coding.dennis;
+//https://www.codewars.com/kata/sum-of-digits-slash-digital-root
 
-public class DigitalRoot {
+/**
+ *
+ */
+public final class DigitalRoot {
+
+    private static final int TEN = 10;
+
+    /**
+     *
+     */
+    private DigitalRoot() {
+
+    }
+
+    /**
+     *
+     * @param inNumber data to apply function.
+     * @return digital root of inNumber.
+     */
+    public static int digitalroot(int inNumber) {
+        int result = inNumber;
+        while (!isOneDigitNumber(result)) {
+            result = root(result);
+        }
+        return result;
+    }
+
+    /**
+     *
+     * @param inNumber data to evaluate.
+     * @return boolean to know if number is only one digit.
+     */
+    private static boolean isOneDigitNumber(int inNumber) {
+        int digitCount = String.valueOf(inNumber).length();
+        return digitCount == 1;
+    }
+
+    /**
+     *
+     * @param inNumber data to know a partial root
+     * @return partial root (it can be more than 2 digits length).
+     */
+    private static int root(int inNumber) {
+        return isOneDigitNumber(inNumber) ? inNumber : inNumber % TEN + root(inNumber / TEN);
+    }
 }
--- src/test/java/org/fundacionjala/coding/dennis/DigitalRootTest.java
+++ src/test/java/org/fundacionjala/coding/dennis/DigitalRootTest.java
@@ -1,5 +1,56 @@
-import static org.junit.Assert.*;
+package org.fundacionjala.coding.dennis;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for Digital Root Kata.
+ */
 public class DigitalRootTest {
 
+    private int data;
+    private static final int ONE_DIGIT = 5;
+    private static final int IN_TWO_DIGITS = 43;
+    private static final int OUT_TWO_DIGITS_TEST = 7;
+    private static final int IN_TWO_DIGITS_REC = 96;
+    private static final int OUT_TWO_DIGITS_REC_TEST = 6;
+    private static final int IN_MORE_DIGITS = 493193;
+    private static final int OUT_MORE_DIGITS = 2;
+
+    /**
+     * If input is one digit, output is the same digit.
+     */
+    @Test
+    public void testOneDigit() {
+        data = ONE_DIGIT;
+        assertEquals(ONE_DIGIT, DigitalRoot.digitalroot(data));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testTwoDigitsOutputOneDigit() {
+        data = IN_TWO_DIGITS;
+        assertEquals(OUT_TWO_DIGITS_TEST, DigitalRoot.digitalroot(data));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testTwoDigitsOutputMoreThanOneDigit() {
+        data = IN_TWO_DIGITS_REC;
+        assertEquals(OUT_TWO_DIGITS_REC_TEST, DigitalRoot.digitalroot(data));
+    }
+
+    /**
+     *
+     */
+    @Test
+    public void testMoreThanTwoDigits() {
+        data = IN_MORE_DIGITS;
+        assertEquals(OUT_MORE_DIGITS, DigitalRoot.digitalroot(data));
+    }
 }
