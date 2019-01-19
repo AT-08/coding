672257f45920becbf21f9511e5823d08ec68f3dc
RogerAlvarezM <roger.alvarez@fundacion-jala.org>
2019-01-18 20:53:24 -0400

    refactor

--- src/main/java/org/fundacionjala/coding/roger/TresEnRaya/Table.java
+++ src/main/java/org/fundacionjala/coding/roger/TresEnRaya/Table.java
@@ -11,65 +11,69 @@ public final class Table {
      * Empty constructor.
      */
     private Table() {
-        table = new String[SIZETABLE][SIZETABLE];
+        table = new String[][]{{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
     }
 
     /**
-     *
      * @param symbol symbol of player.
-     * @param row row of position.
-     * @param column column of position.
+     * @param pos    position.
      */
-
-    public void setPosition(final String symbol, final int row, final int column) {
-        table[row][column] = symbol;
-        verifyWinner(symbol);
+    public void setPosition(final String symbol, final String pos) {
+        String[] position = pos.split("");
+        table[Integer.parseInt(position[0])][Integer.parseInt(position[1])] = symbol;
     }
 
     /**
-     *
-     * @param symbol symbol of player.
+     * @return .
      */
-
-    public void verifyWinner(final String symbol) {
-        //verifyRows(symbol);
-        //verifyColumns(symbol);
-        //verifyDiagonals(symbol);
+    public boolean verifyWinner() {
+        return (verifyRows() || verifyColumns() || verifyDiagonals());
     }
 
     /**
-     *
-     * @param symbol symbol of player.
      * @return boolean is full Row.
      */
-    public boolean verifyRows(final String symbol) {
-        boolean fullRow = false;
-
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table.length; j++) {
-                if (table[i][j].equals(symbol)) {
-                    fullRow = true;
-                }
+    public boolean verifyRows() {
+        for (int i = 0; i < SIZETABLE; i++) {
+            if (table[i][0].equals(table[i][1]) && (table[i][0].equals(table[i][2]))) {
+                return true;
             }
         }
-        return fullRow;
+        return false;
     }
 
     /**
-     * @param symbol symbol of player.
      * @return boolean is full Column.
      */
-    public boolean verifyColumns(final String symbol) {
-        boolean fullColumn = false;
-        return fullColumn;
+    public boolean verifyColumns() {
+        for (int j = 0; j < SIZETABLE; j++) {
+            if (table[0][j].equals(table[1][j]) && (table[0][j].equals(table[2][j]))) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
-     * @param symbol symbol of player.
      * @return boolean is full diagonal.
      */
-    public boolean verifyDiagonals(final String symbol) {
+    public boolean verifyDiagonals() {
         boolean fullDiagonal = false;
         return fullDiagonal;
+    }
+
+    /**
+     * Method for print table.
+     */
+    public void showTable() {
+        System.out.println("- - - - - - - -");
+        for (int i = 0; i < SIZETABLE; i++) {
+            System.out.print("|");
+            for (int j = 0; j < SIZETABLE; j++) {
+                System.out.print(table[i][j] + "|");
+            }
+            System.out.println();
+            System.out.println("- - - - - - - -");
+        }
     }
 }
